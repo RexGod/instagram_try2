3734ef871eb2adf1f43ad5184b8f85a007b7b407
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-02 18:10:31 +0430

    add Videoplayer for control video and ...

---
+++ lib/widget/videoPlayerWidget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+import 'basicOverlayWidget.dart';
+
+class VideoPlayerWidget extends StatelessWidget {
+  final VideoPlayerController controller;
+
+  const VideoPlayerWidget({
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return controller != null && controller.value.isInitialized
+        ? Container(alignment: Alignment.center, child: buildVideo())
+        : Container(
+            height: 200,
+            child: Center(child: CircularProgressIndicator()),
+          );
+  }
+
+  Widget buildVideo() => Stack(
+        children: [
+          buildVideoPlayer(),
+          Positioned.fill(
+            child: BasicOverlayWidget(controller: controller),
+          )
+        ],
+      );
+
+  Widget buildVideoPlayer() => AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: VideoPlayer(controller));
+}
