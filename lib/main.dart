172c25cf331e256989cf56998fd6081ccc649775
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-02 18:06:30 +0430

    test video widget and wrap listView in SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './widget/Storie.dart';
 import './widget/Post.dart';
 import 'direct.dart';
+import 'widget/video.dart';
 
 void main() => runApp(Instagram());
 
@@ -35,7 +36,7 @@ class _InstagramHomePAgeState extends State<InstagramHomePAge> {
     Post('reza', 'assets/images/collins-lesulie-rN8YED0SVZA-unsplash.jpg',
         'assets/images/collins-lesulie-rN8YED0SVZA-unsplash.jpg'),
     Post('sanaz', 'assets/images/keith-tanner-pXzblCBezww-unsplash.jpg',
-        'assets/images/keith-tanner-pXzblCBezww-unsplash.jpg')
+        'assets/images/keith-tanner-pXzblCBezww-unsplash.jpg'),
   ];
 
   void selectPage(BuildContext ctx) {
@@ -44,59 +45,67 @@ class _InstagramHomePAgeState extends State<InstagramHomePAge> {
     }));
   }
 
+  void selectPage2(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return MyVid();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.fromLTRB(7, 25, 5, 7),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Instagram',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.2,
-                        fontStyle: FontStyle.italic)),
-                IconButton(
-                  onPressed: (() => selectPage(context)),
-                  icon: Icon(Icons.send),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(7, 25, 5, 7),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Instagram',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.2,
+                          fontStyle: FontStyle.italic)),
+                  IconButton(
+                    onPressed: (() => selectPage(context)),
+                    icon: Icon(Icons.send),
+                  ),
+                ],
+              ),
+              Container(
+                height: 100,
+                width: double.maxFinite,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                        child: Storie(
+                            stories[index].url, stories[index].userName));
+                  },
+                  itemCount: stories.length,
                 ),
-              ],
-            ),
-            Container(
-              height: 100,
-              width: double.maxFinite,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                      child:
-                          Storie(stories[index].url, stories[index].userName));
-                },
-                itemCount: stories.length,
               ),
-            ),
-            Divider(
-              height: 0,
-              thickness: 0.7,
-              color: Colors.grey[300],
-            ),
-            Container(
-              height: 420,
-              child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                itemBuilder: (context, index) {
-                  return Post(posts[index].userName, posts[index].avatar,
-                      posts[index].media);
-                },
-                itemCount: posts.length,
+              Divider(
+                height: 0,
+                thickness: 0.7,
+                color: Colors.grey[300],
               ),
-            ),
-          ],
+              Container(
+                height: 425,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (context, index) {
+                    return Post(posts[index].userName, posts[index].avatar,
+                        posts[index].media);
+                  },
+                  itemCount: posts.length,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomAppBar(
