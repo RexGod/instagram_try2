70c80a489721d705c5870e787828f80b891e1967
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-17 00:02:31 +0430

    remove home page data in main file and add route

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import './widget/Storie.dart';
-import './widget/Post.dart';
+import 'package:instagram_try2/screen/Favorite.dart';
+import 'package:instagram_try2/screen/Tab.dart';
+import 'package:instagram_try2/screen/home.dart';
+import 'package:instagram_try2/screen/profile.dart';
 import 'screen/direct.dart';
+import 'screen/explorer.dart';
 
 void main() => runApp(Instagram());
 
@@ -10,96 +13,20 @@ class Instagram extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: InstagramHomePAge(),
-      theme: ThemeData(fontFamily: 'openSans'),
-    );
-  }
-}
-
-class InstagramHomePAge extends StatefulWidget {
-  @override
-  State<InstagramHomePAge> createState() => _InstagramHomePAgeState();
-}
-
-class _InstagramHomePAgeState extends State<InstagramHomePAge> {
-  List<Storie> stories = [
-    Storie('assets/images/andrei-caliman-YIs6K1NEdig-unsplash.jpg', 'zahra'),
-    Storie('assets/images/keith-tanner-pXzblCBezww-unsplash.jpg', 'GOD'),
-    Storie('assets/images/moise-m-LByIP24-WYc-unsplash.jpg', 'reza'),
-    Storie('assets/images/hans-isaacson-Eiei7cTji0U-unsplash.jpg', 'sara'),
-    Storie('assets/images/collins-lesulie-rN8YED0SVZA-unsplash.jpg', 'amo'),
-    Storie('assets/images/keith-tanner-pXzblCBezww-unsplash.jpg', 'sanaz'),
-  ];
-
-  List<Post> posts = [
-    Post('reza', 'assets/images/collins-lesulie-rN8YED0SVZA-unsplash.jpg',
-        'assets/images/collins-lesulie-rN8YED0SVZA-unsplash.jpg'),
-    Post('sanaz', 'assets/images/keith-tanner-pXzblCBezww-unsplash.jpg',
-        'assets/images/keith-tanner-pXzblCBezww-unsplash.jpg')
-  ];
-
-  void selectPage(BuildContext ctx) {
-    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
-      return Direct();
-    }));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.fromLTRB(7, 25, 5, 7),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Instagram',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.2,
-                        fontStyle: FontStyle.italic)),
-                IconButton(
-                  onPressed: (() => selectPage(context)),
-                  icon: Icon(Icons.send),
-                ),
-              ],
-            ),
-            Container(
-              height: 100,
-              width: double.maxFinite,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                      child:
-                          Storie(stories[index].url, stories[index].userName));
-                },
-                itemCount: stories.length,
-              ),
-            ),
-            Divider(
-              height: 0,
-              thickness: 0.7,
-              color: Colors.grey[300],
-            ),
-            Container(
-              height: 420,
-              child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                itemBuilder: (context, index) {
-                  return Post(posts[index].userName, posts[index].avatar,
-                      posts[index].media);
-                },
-                itemCount: posts.length,
-              ),
-            ),
-          ],
-        ),
-      ),
-      
+      theme: ThemeData(
+          fontFamily: 'openSans',
+          primarySwatch: Colors.deepPurple,
+          accentColor: Colors.purpleAccent,
+          canvasColor: Color.fromARGB(216, 248, 246, 246)),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => TabsScreen(),
+        '/home': (context) => HomeScreen(),
+        '/favoriteScreen': (context) => FavortieScreen(),
+        '/profileScreen': (context) => ProfileScreen(),
+        '/explorerScreen': (context) => ExplorerScreen(),
+        '/directMessage': (context) => Direct(),
+      },
     );
   }
 }
