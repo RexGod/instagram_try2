c2619b864c5222e95cd15fbb69c459d655cf4343
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-17 00:03:29 +0430

    add navigation bar on bottom of screen and switch page

---
+++ lib/screen/Tab.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import './profile.dart';
+import './explorer.dart';
+import './Favorite.dart';
+import './home.dart';
+
+// ignore: use_key_in_widget_constructors
+class TabsScreen extends StatefulWidget {
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> pages = [
+    {'page': HomeScreen()},
+    {'page': ExplorerScreen()},
+    {'page': FavortieScreen()},
+    {'page': ProfileScreen()},
+  ];
+  int _selectPageIndex = 0;
+  void _selectPage(int index) {
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_selectPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          elevation: 20,
+          backgroundColor: Theme.of(context).canvasColor,
+          // ignore: deprecated_member_use
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: const Color.fromARGB(255, 158, 158, 156),
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          iconSize: 25,
+          currentIndex: _selectPageIndex,
+          items: [
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.home_rounded), label: ''),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.search), label: ''),
+            _selectPageIndex == 2
+                ? const BottomNavigationBarItem(
+                    icon: Icon(Icons.favorite), label: '')
+                : const BottomNavigationBarItem(
+                    icon: Icon(Icons.favorite_border), label: ''),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.star), label: '')
+          ]),
+    );
+  }
+}
