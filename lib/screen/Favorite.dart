954f4cf5c0b854219fc4e439ad718ac9fd31195a
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-17 00:03:58 +0430

    add new screen

---
+++ lib/screen/Favorite.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FavortieScreen extends StatelessWidget {
+  static const routeName = '/favoriteScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
