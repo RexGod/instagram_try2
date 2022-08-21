2ee9779a2670d46cb9a5467ad88a551615494ce5
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-21 22:41:49 +0430

    adding album list view

--- lib/widget/album_list_item.dart
+++ lib/widget/album_list_item.dart
@@ -3,16 +3,38 @@ import 'package:flutter/material.dart';
 import '../screen/albumImageScreen.dart';
 
 class AlbumListItem extends StatelessWidget {
-  final int id;
-  AlbumListItem({required this.id});
-  
-   void tansferData(BuildContext context) {
-    Navigator.of(context).pushNamed(AlbumImage.routeName, arguments: id);
-  } 
+  final String id;
+  final String urlImage;
+  final String title;
+  AlbumListItem(
+      {required this.id, required this.urlImage, required this.title});
+
+  void tansferData(BuildContext context) {
+    Navigator.of(context).pushNamed(AlbumImage.routeName, arguments: urlImage);
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        onTap: () => tansferData(context));
+        onTap: () => tansferData(context),
+        child: Stack(children: [
+          Image.network(urlImage),
+          Positioned(
+            top: 30,
+            right: 30,
+            left: 30,
+            child: Text(
+              title,
+              softWrap: true,
+              overflow: TextOverflow.fade,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+                backgroundColor: Colors.grey[800],
+                color: Colors.white,
+              ),
+            ),
+          )
+        ]));
   }
 }
-
--- lib/widget/explorer_items.dart
+++ lib/widget/explorer_items.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import '../screen/album_List_Screen.dart';
 
 class ExplorerItems extends StatelessWidget {
-  final int id;
+  final String id;
   final String urlImage;
-
-  ExplorerItems({required this.id, required this.urlImage});
+  final String title;
+  ExplorerItems(
+      {required this.id, required this.urlImage, required this.title});
   void tansferData(BuildContext context) {
     Navigator.of(context).pushNamed(AlbumListScreen.routeName, arguments: id);
   }
@@ -13,6 +14,31 @@ class ExplorerItems extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        onTap: () => tansferData(context), child: Image.network(urlImage));
+        onTap: () => tansferData(context),
+        child: Stack(
+          children: [
+            Image.network(
+              urlImage,
+              width: 200,
+              height: 200,
+            ),
+            Positioned(
+              top: 30,
+              right: 30,
+              left: 30,
+              child: Text(
+                title,
+                softWrap: true,
+                overflow: TextOverflow.fade,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                  backgroundColor: Colors.grey[800],
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
