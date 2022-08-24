ee020d8a5f6520f84b37017c28b8333a092b553d
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-24 08:43:44 +0430

    changing passing data to map String key and object valus

--- lib/screen/album_list_screen.dart
+++ lib/screen/album_list_screen.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import '../widget/album_list_item.dart';
 
+// ignore: use_key_in_widget_constructors
 class AlbumListScreen extends StatefulWidget {
   static const routeName = '/AlbumListScreen';
 
@@ -12,6 +13,7 @@ class AlbumListScreen extends StatefulWidget {
 
 class _AlbumListScreenState extends State<AlbumListScreen> {
   List albumSearch = List.empty();
+  List albumphoto = [];
   bool _loading = true;
   @override
   void initState() {
@@ -26,71 +28,54 @@ class _AlbumListScreenState extends State<AlbumListScreen> {
         child: CircularProgressIndicator(),
       );
     }
-    final routeArg = ModalRoute.of(context)?.settings.arguments;
-    final albumId = routeArg.toString();
+    final routeArg =
+        ModalRoute.of(context)?.settings.arguments as Map<String, Object>;
+    final albumId = routeArg['id'].toString();
     final selectedAlbum = albumSearch.where((element) {
-      return albumId == element['Explorer_itemId'];
+      return element['id'].toString().contains(albumId);
     }).toList();
     return Scaffold(
       appBar: AppBar(
+        title: Text(selectedAlbum[int.parse(albumId) - 1]['title']),
         backgroundColor: Theme.of(context).canvasColor,
-        elevation: 5,
+        elevation: 0,
         // ignore: deprecated_member_use
         foregroundColor: Colors.black,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.grey[700],
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10))),
-                margin: const EdgeInsets.symmetric(vertical: 15),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8, horizontal: 100),
-                child: Text(
-                  selectedAlbum[int.parse(albumId) - 1]['Album_tite'],
-                  style: const TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                  softWrap: true,
-                  overflow: TextOverflow.fade,
-                ),
-              ),
-            ),
-            Container(
-              height: 700,
-              width: 400,
-              child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                      maxCrossAxisExtent: 210,
-                      childAspectRatio: 1,
-                      mainAxisExtent: 145),
-                  itemCount: albumSearch.length,
-                  itemBuilder: ((context, index) {
-                    return AlbumListItem(
-                      id: selectedAlbum[index]['id'],
-                      urlImage: selectedAlbum[index]['Image_album'],
-                      title: selectedAlbum[index]['Album_tite'],
-                    );
-                  })),
-            )
-          ],
-        ),
-      ),
+          child: Container(
+        margin: const EdgeInsets.only(top: 20),
+        height: 700,
+        width: 400,
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 300,
+                childAspectRatio: 1,
+                mainAxisExtent: 200),
+            itemCount: albumSearch.length,
+            itemBuilder: ((context, index) {
+              print(albumSearch.length);
+              return AlbumListItem(
+                id: selectedAlbum[index]['id'],
+                urlImage: albumphoto[index]['url'],
+                title: selectedAlbum[index]['title'],
+              );
+            })),
+      )),
     );
   }
 
   void _setData() async {
-    var url2 = Uri.https('6301385a9a1035c7f8ffb7fb.mockapi.io',
-        '/instaDUMMYDATA/api/v01/Explorer_items/1/Album_items');
+    var url2 = Uri.https('jsonplaceholder.typicode.com', '/albums');
     var response2 = await http.get(url2);
     var jsonResponse2 = convert.jsonDecode(response2.body);
+    var url = Uri.https('jsonplaceholder.typicode.com', '/photos');
+    var response = await http.get(url);
+    var jsonResponse = convert.jsonDecode(response.body);
 
     setState(() {
       albumSearch = jsonResponse2 as List;
+      albumphoto = jsonResponse as List;
       _loading = false;
     });
   }
